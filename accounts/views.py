d1d8c15e2a5ee9a4312740b6109311b98ab41258
Seteshy <gamer.7cairo@gmail.com>
2025-11-10 20:08:35 -0300

    Implementa views de autenticação e registro de usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+from django.contrib import messages
 
-# Create your views here.
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, 'Usuário ou senha incorretos.')
+    return render(request, 'accounts/login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Cadastro realizado com sucesso!')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
