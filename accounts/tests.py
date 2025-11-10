268fc04a111c966f67302e19ab9f0a559a4fa130
Seteshy <gamer.7cairo@gmail.com>
2025-11-10 20:58:43 -0300

    Adiciona teste de criação de usuário e finaliza o CRUD inicial de login

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(username='teste', password='123')
+        self.assertEqual(user.username, 'teste')
