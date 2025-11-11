a754cca43af049f08aaa6b5b88e060b29a9a0e7b
Seteshy <gamer.7cairo@gmail.com>
2025-11-10 23:52:33 -0300

    Adiciona a funcionalidade de Recuperar a senha

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('', views.login_usuario, name='login'),
+    path('logout/', views.logout_usuario, name='logout'),
     path('register/', views.register_user, name='register'),
     path('profile/', views.profile, name='profile'),
+
+    # Recuperação de senha
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- login_project/settings.py
+++ login_project/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuração básica de envio de e-mails (modo console)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'popcorre1711@gmail.com'
