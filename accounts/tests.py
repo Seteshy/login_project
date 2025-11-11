cb4c1b43719bef63731346700a35aa32d0c12115
Seteshy <gamer.7cairo@gmail.com>
2025-11-11 02:31:00 -0300

    Apresentação

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import User
 
 class UserTest(TestCase):
     def test_create_user(self):
-        user = User.objects.create_user(username='teste', password='123')
+        user = User.objects.create_user(username='teste', password='1234')
         self.assertEqual(user.username, 'teste')
+        self.assertTrue(user.check_password('1234'))
