188fb10105406571e3df2f7df6f80708ba3afdba
Seteshy <gamer.7cairo@gmail.com>
2025-11-23 23:54:47 -0300

    Criação dos modelos Produto e Carrinho

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Carrinho(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Carrinho de {self.usuario.username}"
+
+class ItemCarrinho(models.Model):
+    carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.produto.nome} x{self.quantidade}"
