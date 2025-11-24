b98753c1e8879e6d6ee2e4709dbb6e3edb283bc6
Seteshy <gamer.7cairo@gmail.com>
2025-11-24 14:54:57 -0300

    Criação Urls produtos

---
+++ produtos/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_produtos, name='lista_produtos'),
+    path('criar/', views.criar_produto, name='criar_produto'),
+    path('deletar/<int:produto_id>/', views.deletar_produto, name='deletar_produto'),
+
+    # CARRINHO
+    path('carrinho/', views.ver_carrinho, name='ver_carrinho'),
+    path('carrinho/add/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
+    path('carrinho/remove/<int:item_id>/', views.remover_do_carrinho, name='remover_do_carrinho'),
+]
